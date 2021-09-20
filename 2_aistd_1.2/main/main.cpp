528649da00df262036a7ba44442a084515052b90
AnnaSluzhevskaya <59249261+ielana2@users.noreply.github.com>
2021-09-20 18:06:38 +0300

    Add files via upload

---
+++ 2_aistd_1.2/main/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "RedBlackTree.h"
+
+int main()
+{
+    RBTree<int, char>* RB = new RBTree<int, char>();
+
+    cout << "first test" << endl;
+    RB->insert(2, 'a');
+    RB->insert(3, 'b');
+    RB->print();
+
+    cout << "second test" << endl;
+    RB->remove(3);
+    RB->print();
+
+    cout << "third test" << endl;
+    RB->insert(1, 'c');
+    RB->print();
+}
